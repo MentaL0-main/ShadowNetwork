bd8bda1523170eb9a5ed3ca9f237c3413435f3c6
Miraly Ruziev <obilev0000@gmail.com>
2026-03-15 00:12:00 +0300

    feat: Create tun interface

--- include/ShadowNetwork/ShadowNetwork.hpp
+++ include/ShadowNetwork/ShadowNetwork.hpp
@@ -12,6 +12,9 @@ private:
   std::string white_address_;
   void choose_white_address();
 
+  int tun_fd_ = -1;
+  void init_tun();
+
   bool try_connect(const std::string &url);
 };
 
--- src/ShadowNetwork.cpp
+++ src/ShadowNetwork.cpp
@@ -1,19 +1,28 @@
 #include <ShadowNetwork/ShadowNetwork.hpp>
 
+#include <fcntl.h>
 #include <fstream>
 #include <iostream>
+#include <linux/if.h>
+#include <linux/if_tun.h>
 #include <ostream>
 #include <stdexcept>
+#include <string.h>
 #include <string>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
 #include <curl/curl.h>
 
 namespace sn {
 
-void ShadowNetwork::run() { choose_white_address(); }
+void ShadowNetwork::run() {
+  choose_white_address();
+  init_tun();
+}
 
 void ShadowNetwork::choose_white_address() {
-  std::cout << "[*] Choosing a white address..." << std::flush;
+  std::cout << "[*] Choising a white address..." << std::flush;
 
   std::ifstream file;
   file.open("../whitelist.txt");
@@ -35,6 +44,33 @@ void ShadowNetwork::choose_white_address() {
   }
 
   std::cout << "OK: " << white_address_ << std::endl;
+}
+
+void ShadowNetwork::init_tun() {
+  std::cout << "[*] Creating tun interface..." << std::flush;
+
+  ifreq ifr;
+  int err;
+
+  if ((tun_fd_ = open("/dev/net/tun", O_RDWR)) < 0) {
+    throw std::runtime_error("Failed to open /dev/net/tun");
+  }
+
+  std::string tun_name = "tun%d";
+
+  memset(&ifr, 0, sizeof(ifr));
+  ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+  if (tun_name.c_str()) {
+    strncpy(ifr.ifr_name, tun_name.c_str(), IFNAMSIZ);
+  }
+
+  if ((err = ioctl(tun_fd_, TUNSETIFF, (void *)&ifr)) < 0) {
+    close(tun_fd_);
+    throw std::runtime_error("Error ioctl TUNSETIFF. Maybe try with root?");
+  }
+
+  tun_name = ifr.ifr_name;
+  std::cout << "OK: " << tun_name << std::endl;
 }
 
 bool ShadowNetwork::try_connect(const std::string &url) {
